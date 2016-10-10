afa329daac35705ecf4d6d823b812272ba2cd26a
Evan Mu <190567268@qq.com>
2016-10-10 21:31:16 +0800

    create template of simple substitution cipher

---
+++ SimpleSubCipher/SImpleSubCipher.py
@@ -0,0 +1,27 @@
+# Simple Substitution Cipher
+import pyperclip
+import sys
+import random
+
+LETTERS = 'ABCDEFGGHIJKLMNOPQRSTUVWXYZ'
+
+def main():
+    pass
+
+def check_valid_key(key):
+    pass
+
+def encrypt_message(key, message):
+    pass
+
+def decrypt_message(key, message):
+    pass
+
+def translate_message(key, message, mode):
+    pass
+
+def get_random_key():
+    pass
+
+if __name__ == '__main__':
+    main()
