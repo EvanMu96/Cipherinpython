0ee9a1cbade1d6a84aea5171cc8efadca9e116d5
Evan Mu <190567268@qq.com>
2016-10-11 16:27:09 +0800

    Simple Substitution Cipher

---
+++ Affine_Cipher/HackAffine.py
@@ -0,0 +1,9 @@
+import pyperclip
+import AffineCipher
+import detect_english
+import cryptomath
+
+SILENT_MODE = False
+
+def main():
+    my_message = """U&'<3dJ^Gjx'-3^MS'Sj0jxuj'G3'%j'<mMMjS'g{GjMMg9j{G'g"'gG'<3^MS'Sj<jguj'm'P^dm{'g{G3'%jMgjug{9'GPmG'gG'-m0'P^dm{LU'5&Mm{'_^xg{9"""
--- SimpleSubCipher/SImpleSubCipher.py
+++ SimpleSubCipher/SImpleSubCipher.py
@@ -6,19 +6,63 @@ import random
 LETTERS = 'ABCDEFGGHIJKLMNOPQRSTUVWXYZ'
 
 def main():
-    pass
+    my_message = """PLZ REPLACE YOUR PLAIN TEXT"""
+    my_key = 'HKJFUIOWHJKDJFSDBBEUIFBUSK'
+    my_mode = 'encrypt'
+
+    # Check my_key is valid or not
+    check_valid_key(my_key)
+
+    if my_mode=='encrypt':
+        translated = encrypt_message(my_key, my_message)
+    elif my_mode=='decrypt':
+        translated = decrypt_message(my_key, my_message)
+    print('Using key %s' % my_key)
+    print('THe %sed message is :' % my_mode)
+    print(translated)
+    print()
+    print('This message has copied to clipboard')
+    
 
 def check_valid_key(key):
-    pass
+    key_list = list(key)
+    letters_list = list(LETTERS)
+    key_list.sort()
+    letters_list.sort()
+    if key_list != letters_list:
+        sys.exit('There is an error in the key or symbol set')
+
 
 def encrypt_message(key, message):
-    pass
+    return translate_message(key, message, 'encrypt')
+
 
 def decrypt_message(key, message):
-    pass
+    return translate_message(key, message, 'decrypt')
+
 
 def translate_message(key, message, mode):
-    pass
+    translated = ''
+    charsA = LETTERS
+    charsB = key
+    if mode=='decrypt':
+        # For decrypting, we can use the same code as encrypting.
+        # We just need to swap where the key and LETTERS strings are used.
+        charsA, charsB = charsB, charsA
+    # Loop each symbol in message
+    for symbol in message:
+        if symbol.upper() in charsA:
+            # encrypt/decrypt the symbol
+            symIndex = charsA.find(symbol.upper())
+            if symbol.isupper():
+                translated += charsB[symIndex].upper()
+            else:
+                translated += charsB[symIndex].lower()
+        else:
+            translated +=symbol
+
+    return translated
+
 
 def get_random_key():
     pass
